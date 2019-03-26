f9e5951c19b9659cc76283b0e45edc5e43c72cb3
Joshua Arriola <joshuapatrickarriola@gmail.com>
2019-03-26 10:13:29 -0700

    Update reverse Linked List

--- reverse_linked_list.js
+++ reverse_linked_list.js
@@ -1,15 +1,15 @@
-var reverseList = function(head) {
+var reverseList = function (head) {
   if (!head) return null;
   let currentNode = head;
   let next = currentNode.next;
   let prev = ListNode(0);
-  
-  while(currentNode){
+
+  while (currentNode) {
     next = currentNode.next;
     currentNode.next = prev;
     prev = currentNode;
     currentNode = next;
   }
-  
+
   return prev;
 };
