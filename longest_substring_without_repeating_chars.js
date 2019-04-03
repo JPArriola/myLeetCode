709a6e9fa8e884aeb2568ad1a1e0e3a01e9f1cb7
Joshua Arriola <joshuapatrickarriola@gmail.com>
2019-04-03 11:51:34 -0700

    Second solution to longest substring without repeating chars

--- longest_substring_without_repeating_chars.js
+++ longest_substring_without_repeating_chars.js
@@ -1,13 +1,24 @@
 function lengthOfLongestSubstring(s) {
+  //     const map = {};
+  //     var left = 0;
+
+  //     return s.split('').reduce((max, v, i) => {
+  //         left = map[v] >= left ? map[v] + 1 : left;
+  //         map[v] = i;
+  //         return Math.max(max, i - left + 1);
+  //     }, 0);
+
   const map = {};
-  var left = 0;
+  let max = 0;
 
-  return s.split('').reduce((max, v, i) => {
-    console.log(max, v, i)
-    left = map[v] >= left ? map[v] + 1 : left;
+  for (let i = 0, j = 0; i < s.length; i++) {
+    if (map[s[i]] !== undefined) {
+      j = Math.max(j, map[s[i]] + 1)
+    }
 
-    console.log(map[v], left)
-    map[v] = i;
-    return Math.max(max, i - left + 1);
-  }, 0);
+    map[s[i]] = i;
+    max = Math.max(max, (i - j) + 1);
+  }
+
+  return max;
 }
