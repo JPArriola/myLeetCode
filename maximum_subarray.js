2ef1153ca3466b64fc4a7727f295f63798c19374
Joshua Arriola <joshuapatrickarriola@gmail.com>
2019-04-18 21:01:06 -0700

    Finish maximum subarray

---
+++ maximum_subarray.js
@@ -0,0 +1,10 @@
+var maxSubArray = function (nums) {
+  var prev = 0;
+  var max = nums[0];
+
+  for (var i = 0; i < nums.length; i++) {
+    prev = Math.max(prev + nums[i], nums[i]);
+    max = Math.max(max, prev);
+  }
+  return max;
+};
